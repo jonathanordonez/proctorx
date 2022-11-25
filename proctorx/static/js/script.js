0078b40e53d954585e0244413c51523bacf7981a
Jonathan Ordonez <jordonez@diligent.com>
2022-11-25 00:20:29 -0500

    fixed the elements background colors not displaying entirely by adding wrappers to each element

---
+++ proctorx/static/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function linkReservationSelectOptions() {
+    if (document.location.pathname == '/student/reservation') {
+        reservationOptions = document.querySelectorAll(".reservation-option")
+        // option = 
+        // for in reservationOptions {
+        //     console.log(option)
+        // }
+        // console.log(reservationOptions)
+    }
+}
+
