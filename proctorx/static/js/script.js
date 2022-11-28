da633ef893224ec1076b256d4b1d2cd66fa561ad
Jonathan Ordonez <jordonez@diligent.com>
2022-11-27 20:03:19 -0500

    finished footer, cart count and settings drop down menu

--- proctorx/static/js/script.js
+++ proctorx/static/js/script.js
@@ -1,11 +1,34 @@
-window.onload = function linkReservationSelectOptions() {
-    if (document.location.pathname == '/student/reservation') {
-        reservationOptions = document.querySelectorAll(".reservation-option")
-        // option = 
-        // for in reservationOptions {
-        //     console.log(option)
-        // }
-        // console.log(reservationOptions)
-    }
-}
+// window.onload = function linkReservationSelectOptions() {
+//     if (document.location.pathname == '/student/reservation') {
+//         reservationOptions = document.querySelectorAll(".reservation-option")
+//         // option = 
+//         // for in reservationOptions {
+//         //     console.log(option)
+//         // }
+//         // console.log(reservationOptions)
+//     }
+// }
 
+window.onload = addEventListeners
+
+function addEventListeners() {
+    // Drop-down menu listener
+    let userSettings = document.querySelector('.user-settings')
+    let dropDownMenu = document.querySelector('.user-settings-drop-down-menu')
+    userSettings.onclick = function showHideDropDownMenu() {
+        if (dropDownMenu.style.display == 'none' || dropDownMenu.style.display == '') {
+            dropDownMenu.style.display = 'block';
+        }
+        else {
+            dropDownMenu.style.display = 'none';
+        }
+    }
+    window.addEventListener('click', function hideSettingsMenu(e) {
+        if (dropDownMenu.style.display = 'block' && !userSettings.contains(e.target)) {
+            dropDownMenu.style.display = 'none';
+        }
+    })
+    window.addEventListener('click', function findClick(e) {
+        console.log(e.target)
+    })
+}
